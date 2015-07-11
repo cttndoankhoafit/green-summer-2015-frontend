0f25afea71d5957974573dfd9de3bd4c8071e642
Khanh Nguyen <nguyenkhanh.cantho.1@gmail.com>
2015-07-11 22:09:17 +0700

    Edit base structure

--- mms_frontend/mms_webapp/views/dashboard.py
+++ mms_frontend/mms_webapp/views/dashboard.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
-    template_name = 'adm/base.html'
+	template_name = 'adm/base.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(DashboardView, self).get_context_data(**kwargs)
+
+		context['title'] = u'Bảng điều khiển'
+		context['page_title'] = u'Bảng điều khiển'
+
+		return context
